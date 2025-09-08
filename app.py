13885d87bba18a6cffade24cc2dbdc1da9107cf8
magwindow <antosha.petrakov@yandex.ru>
2025-09-08 15:11:46 +0300

    Added database

--- app.py
+++ app.py
@@ -1,6 +1,58 @@
-from flask import Flask, render_template
+import os
 
+from dotenv import load_dotenv
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_ADDRESS = os.getenv('DB_ADDRESS')
+DB_NAME = os.getenv('DB_NAME')
+
+# Create the extension
+db = SQLAlchemy()
+
+# Create the app
 app = Flask(__name__)
+
+# Configure the Postgres database
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_ADDRESS}/{DB_NAME}'
+
+# Initialize the app with extension
+db.init_app(app)
+
+# Create Model
+from datetime import datetime
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import mapped_column
+
+
+class Category(db.Model):
+    """Категории постов"""
+    __tablename__ = 'category'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150))
+    posts = db.relationship('Post', back_populates='category')
+
+    def __repr__(self):
+        return self.title
+
+
+class Post(db.Model):
+    """Новостные посты"""
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150))
+    content = db.Column(db.Text)
+    created = db.Column(db.DateTime, default=datetime.now())
+    category_id = mapped_column(ForeignKey('category.id'))
+    category = db.relationship('Category', back_populates='posts')
+
+    def __repr__(self):
+        return self.title
 
 
 @app.route('/')
